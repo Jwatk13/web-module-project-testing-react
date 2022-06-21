990c9088efb92cfda0ebed7926b01020b67b0b2c
Joseph Watkins <josephxwatkins@gmail.com>
2022-06-21 16:36:52 -0400

    episode tests complete

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -3,8 +3,29 @@ import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Episode from './../Episode';
 
-test("renders without error", () => { });
+const testEpisode = {
+    summary: "testing",
+    image: null
+}
 
-test("renders the summary test passed as prop", () => { });
+test("renders without error", () => {
+    render(<Episode episode={testEpisode}/>)
+ });
 
-test("renders default image when image is not defined", () => { });
+test("renders the summary test passed as prop", () => {
+    render(<Episode episode={testEpisode}/>)
+
+    const summarySays = screen.queryByText(/testing/i);
+
+    expect(summarySays).toBeInTheDocument();
+    expect(summarySays).toBeTruthy();
+    expect(summarySays).toHaveTextContent("testing");
+ });
+
+test("renders default image when image is not defined", () => {
+    render(<Episode episode={testEpisode} />)
+
+    const imgTest = screen.queryByAltText('https://i.ibb.co/2FsfXqM/stranger-things.png');
+
+    expect(imgTest).toBeInTheDocument();
+ });
